ca631cfa7703a24c853d76fb6a80fd0952551276
Peter Xervi <xervipeter@gmail.com>
2025-11-21 11:04:47 +0300

    Implement return_evens and make_exclamation functions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
 
-def make_exclamation(sentence_list):
-    pass
+def return_evens(sequence):
+    """
+    Returns a list of all even elements from a sequence of integers.
+    """
+    return [num for num in sequence if num % 2 == 0]
+
+def make_exclamation(sentences):
+    """
+    Returns a list of sentences with exclamation marks added at the end.
+    """
+    return [sentence + "!" for sentence in sentences]
